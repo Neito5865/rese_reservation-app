753f0503fe96e5c8493926541a176995d7514f7f
NeitoKomiyama <neito.komi11@gmail.com>
2024-09-06 21:01:49 +0900

    create:bladeファイルの作成

---
+++ src/resources/views/commons/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <div class="hamburger-menu" onclick="toggleMenu(this)">
+        <div class="hamburger-line"></div>
+        <div class="hamburger-line"></div>
+        <div class="hamburger-line"></div>
+    </div>
+    <script>
+        function toggleMenu(element){
+            element.classList.toggle("active");
+        }
+    </script>
+</header>
