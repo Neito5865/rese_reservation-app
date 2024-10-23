1cda7f1feff832be55427d357494547e7c2362fb
NeitoKomiyama <neito.komi11@gmail.com>
2024-10-23 16:46:54 +0900

    edit:決済機能の実装_決済画面の作成

--- src/resources/views/payment/payment.blade.php
+++ src/resources/views/payment/payment.blade.php
@@ -40,7 +40,7 @@
                 </div>
                 <div class="payment-form__group">
                     <div class="payment-form__group-inner">
-                        <label class="payment-form__label" for="amount">金額 (JPY): </label>
+                        <label class="payment-form__label" for="amount">金額を入力</label>
                         <input class="payment-form__input" type="number" name="amount" id="amount">
                     </div>
                     <div class="payment-form__error">
@@ -49,8 +49,29 @@
                         @enderror
                     </div>
                 </div>
-                <div class="payment-form__group">
-                    <div id="card-element"></div>
+                <div class="payment-card-form__content">
+                    <div class="payment-card-form__heading">
+                        <p>カード情報を入力</p>
+                    </div>
+                    <div class="payment-form__group">
+                        <div class="payment-form__group-inner">
+                            <label class="payment-form__label" for="card_number">カード番号</label>
+                            <div id="card-number" class="payment-form__card-form"></div>
+                        </div>
+                    </div>
+                    <div class="payment-form__group">
+                        <div class="payment-form__group-inner">
+                            <label class="payment-form__label" for="card_expiry">有効期限</label>
+                            <div id="card-expiry" class="payment-form__card-form"></div>
+                        </div>
+                    </div>
+                    <div class="payment-form__group">
+                        <div class="payment-form__group-inner">
+                            <label class="payment-form__label" for="card_cvc">セキュリティコード</label>
+                            <div id="card-cvc" class="payment-form__card-form"></div>
+                        </div>
+                    </div>
+                    <div id="card-errors" class="text-danger"></div>
                 </div>
                 <div class="payment-form__btn">
                     <input class="payment-form__btn--submit" type="submit" value="決済を行う">
@@ -64,13 +85,61 @@
     <script>
         var stripe = Stripe('{{ env('STRIPE_KEY') }}');
         var elements = stripe.elements();
-        var card = elements.create('card');
-        card.mount('#card-element');
+
+        var style = {
+            base: {
+                color: '#32325d',
+                fontSmoothing: 'antialiased',
+                fontSize: '16px',
+                '::placeholder': {
+                    color: '#aab7c4'
+                }
+            },
+            invalid: {
+                color: '#fa755a',
+                iconColor: '#fa755a',
+                borderColor: '#fa755a'
+            }
+        };
+
+        var cardNumber = elements.create('cardNumber', {style: style});
+        cardNumber.mount('#card-number');
+        cardNumber.on('change', function(event){
+            var displayError = document.getElementById('card-errors');
+            if (event.error){
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+
+        var cardExpiry = elements.create('cardExpiry', {style: style});
+        cardExpiry.mount('#card-expiry');
+        cardExpiry.on('change', function(event){
+            var displayError = document.getElementById('card-errors');
+            if (event.error){
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
+
+        var cardCvc = elements.create('cardCvc', {style: style});
+        cardCvc.mount('#card-cvc');
+        cardCvc.on('change', function(event){
+            var displayError = document.getElementById('card-errors');
+            if (event.error){
+                displayError.textContent = event.error.message;
+            } else {
+                displayError.textContent = '';
+            }
+        });
 
         var form = document.getElementById('payment-form');
         form.addEventListener('submit', function(event){
             event.preventDefault();
-            stripe.createToken(card).then(function(result){
+
+            stripe.createToken(cardNumber).then(function(result){
                 if (result.error) {
                     // エラー表示
                 } else {
